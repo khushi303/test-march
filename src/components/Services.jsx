9dbade1fc425dfcfc3914de46ada336d8590aa39
khushi303 <maankhushi303@gmail.com>
2024-03-16 16:11:56 +0530

    adding z-index in service cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,14 +9,14 @@ const Services = () => {
                 <h2 className='sm:text-[48px] text-[33px] font-normal leading-[120%] text-white text-center md:mb-4 mb-3'>Our <span className='text-[#BAFF00] '>Services</span></h2>
                 <p className='text-[16px] font-normal text-center text-white leading-[150%]'>Dolor ipsum facilisi facilisis diam mi dis. Et gravida porta quam risus consectetur est <span className='lg:block'>imperdiet. Enim feugiat nisl dolor dui est. </span></p>
                 <div className='flex lg:justify-between justify-center lg:gap-0 gap-4 flex-wrap lg:items-start items-center md:mt-10 mt-6 relative z-20'>
-                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full'>
+                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full relative z-10'>
                         <div className="md:min-h-[318px] h-full border border-solid border-[#FFFFFF29] bg-[url('./assets/images/png/service-box-image.png')] bg-center bg-cover bg-no-repeat sm:py-10 py-7 rounded-[16px] px-[18px] flex items-center justify-start flex-col pl-[18px] pr-[22px] backdrop-blur-[20px] transition-all duration-300 ease-linear group hover:border-[#BAFF00] cursor-pointer">
                             <PentestImg />
                             <h3 className='text-[24px] font-semibold text-white text-center leading-[140%] sm:mt-5 mt-3 mb-2'>PentestImg</h3>
                             <p className='leading-[150%] text-[16px] font-normal text-white opacity-80 text-center'>Cybertise's pentesting enhances digital security with manual 'BlackBox' and 'GreyBox' tests emulating real attacks, offering risk <span className='xl:block'>solutions for diverse systems.</span></p>
                         </div>
                     </div>
-                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full'>
+                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full relative z-10'>
                         <div className="md:min-h-[318px] h-full border border-solid border-[#FFFFFF29] bg-[url('./assets/images/png/service-box-image.png')] bg-center bg-cover bg-no-repeat sm:py-10 py-7 rounded-[16px] px-[18px] flex items-center justify-start flex-col pl-[18px] pr-[22px] backdrop-blur-[20px] transition-all duration-300 ease-linear group hover:border-[#BAFF00] cursor-pointer">
                             <CrowdImg />
                             <h3 className='text-[24px] font-semibold text-white text-center leading-[140%] sm:mt-5 mt-3 mb-2'>Crowd-Sourced Pentests</h3>
@@ -24,7 +24,7 @@ const Services = () => {
                                 identification.</span></p>
                         </div>
                     </div>
-                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full'>
+                    <div className='lg:w-[32.3%] md:w-[48%] sm:w-[80%] w-full relative z-10'>
                         <div className="md:min-h-[318px] h-full border border-solid border-[#FFFFFF29] bg-[url('./assets/images/png/service-box-image.png')] bg-center bg-cover bg-no-repeat sm:py-10 py-7 rounded-[16px] px-[18px] flex items-center justify-start flex-col pl-[18px] pr-[22px] backdrop-blur-[20px] transition-all duration-300 ease-linear group hover:border-[#BAFF00] cursor-pointer">
                             <VrpImg />
                             <h3 className='text-[24px] font-semibold text-white text-center leading-[140%] sm:mt-5 mt-3 mb-2'>VRP</h3>
